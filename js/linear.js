45df0d0aa01599c7e41ef404aa6a3d7e6d0b3be0
Stef <401532@student.fontys.nl>
2020-11-17 11:44:24 +0100

    Fixed spelling mistake and rounded the number

--- js/linear.js
+++ js/linear.js
@@ -7,8 +7,8 @@ sensor.onreading = () => {
     console.log("Acceleration along Z-axis: " + sensor.z);
     // alert("test");
     // document.getElementById("x").textContent="Acceleration along X-axis: " + sensor.x;
-    // document.getElementById("y").textContent="Acceleration along X-axis: " + sensor.y;
-    document.getElementById("z").textContent="Acceleration along X-axis: " + sensor.z;
+    // document.getElementById("y").textContent="Acceleration along Y-axis: " + sensor.y;
+    document.getElementById("z").textContent="Acceleration along Z-axis: " + Math.round(sensor.z * 100) / 100;
 }
 
 sensor.onerror = event => console.log(event.error.name, event.error.message);
